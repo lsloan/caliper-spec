6b3d2ec68aee6393682974e73387b62de1269ea3
Bracken Mosbacker <bmosbacker@imsglobal.org>
2020-06-02 14:37:35 -0700

    add ipr statement respec config

---
+++ respec_shared/iprs.js
@@ -0,0 +1,13 @@
+var _iprs = [
+    {
+        company: "D2L Corporation",
+        electionDate: "March 28, 2020",
+        necessaryClaims: "No",
+        type: "RF RAND (Required Elements)"
+    }, {
+        company: "i-Scream EDU",
+        electionDate: "March 29, 2020",
+        necessaryClaims: "No",
+        type: "RF RAND (Required Elements)"
+    }
+]
